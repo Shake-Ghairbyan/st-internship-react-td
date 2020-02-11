17a2dc43322921f2b408317c41bd2e50bcfe5381
shakeRocketAndRoll <57106379+shakeRocketAndRoll@users.noreply.github.com>
2020-02-11 09:40:03 +0400

    Updates

---
+++ precache-manifest.0a47c4ddf6739efe6938a6d590ded0a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5217c68a49634113c08ba3ba6b2a8837",
+    "url": "/st-internship-react-td/index.html"
+  },
+  {
+    "revision": "aa9e8d321f89a220c952",
+    "url": "/st-internship-react-td/static/css/main.c1f059e3.chunk.css"
+  },
+  {
+    "revision": "5e0b0230e3dfc07ef1b8",
+    "url": "/st-internship-react-td/static/js/2.f8d85107.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/st-internship-react-td/static/js/2.f8d85107.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa9e8d321f89a220c952",
+    "url": "/st-internship-react-td/static/js/main.ee9c3520.chunk.js"
+  },
+  {
+    "revision": "32690e4f8619dbf354c8",
+    "url": "/st-internship-react-td/static/js/runtime-main.287b82c6.js"
+  }
+]);
